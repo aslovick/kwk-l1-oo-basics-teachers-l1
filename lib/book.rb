52757d2c2fbd3cebbb3c91c4ab736f3916e98063
Abigail Slovick <abby.slovick@gmail.com>
2018-06-22 15:33:56 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -2,34 +2,36 @@
 # book.rb
 # Add you Book class here
 class Book
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
 
   def initialize(title)
     @title = title
   end
-  def title
-    @title
-  end 
+  # def title
+  #   @title
+  # end 
   
-  def author=(author)
-    @author = author
-  end 
-  def author
-    @author 
-  end 
+  # def author=(author)
+  #   @author = author
+  # end 
+  # def author
+  #   @author 
+  # end 
   
-  def page_count=(pages)
-    @page_count = pages
-  end 
-  def page_count
-    @page_count
-  end 
+  # def page_count=(pages)
+  #   @page_count = pages
+  # end 
+  # def page_count
+  #   @page_count
+  # end 
   
-  def genre=(genre)
-    @genre = genre
-  end 
-  def genre
-    @genre
-  end 
+  # def genre=(genre)
+  #   @genre = genre
+  # end 
+  # def genre
+  #   @genre
+  # end 
   
   def turn_page
     puts "Flipping the page...wow, you read fast!"
